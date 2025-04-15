2af5be7d655209372726fd57ad732d7f6cf7d131
Heitor Correa <heitorcorrea001@gmail.com>
2025-04-15 05:50:30 -0300

    commit fix totalmente card infinito
    foi arrumado passar um por um, centralizado, e tambem ta funcionando o scroll infinito, e de quebra teste do avaliações

---
+++ clubedatorre-main/aulas/escolinhaparticular/main.js
@@ -0,0 +1,5 @@
+// Script principal da página
+window.addEventListener("load", function () {
+  // Código específico para a página principal, não relacionado ao carrossel
+  console.log("Página carregada com sucesso!");
+}); 
