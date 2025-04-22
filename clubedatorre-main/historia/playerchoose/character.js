d8ca89c10b9e96362b8dfbfa5fae9046f16d8915
Heitor Correa <heitorcorrea001@gmail.com>
2025-04-22 05:09:03 -0300

    quase pronto

--- clubedatorre-main/historia/playerchoose/character.js
+++ clubedatorre-main/historia/playerchoose/character.js
@@ -4,10 +4,16 @@
   // Adicionar evento de clique para cada card
   cards.forEach(card => {
     card.addEventListener('click', () => {
-      // Remover classe 'selected' de todos os cards
-      cards.forEach(c => c.classList.remove('selected'));
-      
-      // Adicionar classe 'selected' ao card clicado
-      card.classList.add('selected');
+      // Verificar se o card clicado já está selecionado
+      if (card.classList.contains('selected')) {
+        // Se já estiver selecionado, remove a classe 'selected'
+        card.classList.remove('selected');
+      } else {
+        // Remover classe 'selected' de todos os cards
+        cards.forEach(c => c.classList.remove('selected'));
+        
+        // Adicionar classe 'selected' ao card clicado
+        card.classList.add('selected');
+      }
     });
   });
